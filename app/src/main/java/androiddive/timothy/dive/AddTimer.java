735770b7f9d90c69c52b2a2cbb40e2fc1ed63995
Tim-Hoff <timchoff@gmail.com>
2014-11-09 18:02:21 -0600

    asdf

---
+++ app/src/main/java/androiddive/timothy/dive/AddTimer.java
@@ -0,0 +1,129 @@
+package androiddive.timothy.dive;
+
+import android.app.Activity;
+import android.app.ActionBar.LayoutParams;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.EditText;
+
+
+public class AddTimer extends Activity implements View.OnClickListener  {
+    private ImageButton bCheck,bCross;
+    private Button bAdd;
+    private LinearLayout lay,nlay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_timer);
+
+
+        bCheck = (ImageButton)findViewById(R.id.buttonCheck);
+        bCheck.setOnClickListener(this);
+        bAdd=(Button) findViewById(R.id.buttonAdd);
+        bAdd.setOnClickListener(this);
+        lay = (LinearLayout) findViewById(R.id.LayTon);
+        nlay= new LinearLayout(this);
+        nlay.setOrientation(LinearLayout.HORIZONTAL);
+        lay.addView(nlay);
+        nlay.setGravity(Gravity.TOP);
+
+
+
+
+    }
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.buttonAdd){
+            // do this with a struct later
+
+            EditText nh= createEditText((String)getText(R.string.dummy_Ihour),10,0,0,0);
+            nlay.addView(nh);
+            nh.setGravity(Gravity.TOP);
+
+
+            EditText col1= createEditText((String)getText(R.string.dummy_col),0,0,0,0);
+            nlay.addView(col1);
+
+            EditText nm= createEditText((String)getText(R.string.dummy_Imin),0,0,0,0);
+            nlay.addView(nm);
+
+            EditText col2= createEditText((String) getText(R.string.dummy_col),0,0,0,0);
+            nlay.addView(col2);
+
+            EditText ns= createEditText((String)getText(R.string.dummy_Isec),0,0,0,0);
+            nlay.addView(ns);
+
+            EditText x = createEditText((String)getText(R.string.dummy_x),35,0,15,0);
+            nlay.addView(x);
+
+            EditText inc= createEditText((String)getText(R.string.dummy_Inc),0,0,0,0);
+            nlay.addView(inc);
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_timer, menu);
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private EditText createEditText(String str,int l,int t,int r,int b)
+    {
+        EditText edittext = new EditText(this);
+        edittext.setText(str);
+        edittext.setTextColor(Color.WHITE);
+        edittext.setTextSize(30);
+        edittext.setMaxLines(1);
+
+        edittext.setTypeface(null, Typeface.NORMAL);
+
+
+        if(edittext.length()==1){
+            edittext.setFilters(new InputFilter[]{new InputFilter.LengthFilter(1)});
+            edittext.setCursorVisible(false);
+            edittext.setFocusable(false);
+            edittext.setFocusableInTouchMode(false);
+            edittext.setFreezesText(true);
+            edittext.setClickable(false);
+        } else {
+            edittext.setFilters(new InputFilter[]{new InputFilter.LengthFilter(2)});
+
+            edittext.setInputType(InputType.TYPE_CLASS_NUMBER);
+        }
+            edittext.setPadding(l,t,r,b);
+        edittext.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        return edittext;
+    }
+
+}
--- app/src/main/java/androiddive/timothy/dive/MainActivity.java
+++ app/src/main/java/androiddive/timothy/dive/MainActivity.java
@@ -1,48 +1,120 @@
 package androiddive.timothy.dive;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.method.KeyListener;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
  * Created by Timothy on 11/4/2014.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button bStart,bStop, db, da;
+    private EditText eh,em,es;
+    private long totalTime,incrementalTime;
+    private CountDownTimer countDownTimer;
+    private KeyListener lh,lm,ls;
+    public int x = 600;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bStart = (Button)findViewById(R.id.buttonStartTimer);
+        bStop = (Button)findViewById(R.id.buttonStopTimer);
+        db = (Button)findViewById(R.id.dummy_button);
+        da = (Button)findViewById(R.id.dummy_add);
+        eh = (EditText) findViewById(R.id.editHour);
+        em = (EditText) findViewById(R.id.editMin);
+        es = (EditText) findViewById(R.id.editSeconds);
 
-//        Button button = (Button) ;
-        findViewById(R.id.dummy_button).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Timer Timer1 = new Timer();
+        bStart.setOnClickListener(this);
+        bStop.setOnClickListener(this);
+        db.setOnClickListener(this);
+        da.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick (View v) {
+        if(v.getId()==R.id.buttonStartTimer){
+
+            setTimer();
+            bStop.setVisibility(View.VISIBLE);
+            bStart.setVisibility(View.GONE);
+            lh=eh.getKeyListener();
+            lm=em.getKeyListener();
+            ls=es.getKeyListener();
+            sl(eh);
+            sl(em);
+            sl(es);
+
+            startTimer();
+        } else if (v.getId() == R.id.buttonStopTimer){
+            countDownTimer.cancel();
+            bStop.setVisibility(View.GONE);
+            bStart.setVisibility(View.VISIBLE);
+            eh.setKeyListener(lh);
+            em.setKeyListener(lm);
+            es.setKeyListener(ls);
+            stl(eh);
+            stl(em);
+            stl(es);
+        } else if(v.getId()==R.id.dummy_button){
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+        } else if(v.getId()==R.id.dummy_add){
+            startActivity(new Intent(MainActivity.this, AddTimer.class));
+        }
+    }
+    private void setTimer() {
+
+        try{
+            totalTime = ((Integer.parseInt(eh.getText().toString())*3600)+
+                    (Integer.parseInt(em.getText().toString())*60)+
+                    (Integer.parseInt(es.getText().toString())))*1000;
+
+        } catch (NumberFormatException nfe){
+            Log.e("nfe", "could not parse" + nfe);
+        }
+
+    }
+    private void startTimer(){
+        countDownTimer = new CountDownTimer(totalTime,500) {
+            @Override
+            public void onTick(long leftTimeInMilliseconds){
+                long sec = leftTimeInMilliseconds/1000;
+
+                eh.setText(String.format("%02d",sec/3600));
+                em.setText(String.format("%02d",(sec/60)%60));
+                es.setText(String.format("%02d",sec%60));
+
             }
-        });
-        findViewById(R.id.dummy_add).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                int a = 0;
 
-                EditText eh = (EditText) findViewById(R.id.editHour);
-                EditText em = (EditText) findViewById(R.id.editHour);
-                EditText es = (EditText) findViewById(R.id.editHour);
-                try{
-                    a = ((Integer.parseInt(eh.getText().toString())*3600)+
-                         (Integer.parseInt(em.getText().toString())*60)+
-                         (Integer.parseInt(es.getText().toString())));
-
-                    //a = (Integer.parseInt(eh.getText().toString()));
-                } catch (NumberFormatException nfe){
-                    Log.e("nfe", "could not parse" + nfe);
-                }
-
-                Timer Timer2 = new Timer(a);
+            @Override
+            public void onFinish() {
+                bStop.setVisibility(View.GONE);
+                bStart.setVisibility(View.VISIBLE);
             }
-        });
+        }.start();
+
+    }
+    public void sl(EditText ek){
+        ek.setKeyListener(null);
+        ek.setClickable(false);
+        ek.setCursorVisible(false);
+        ek.setFocusable(false);
+        ek.setFocusableInTouchMode(false);
+    }
+    public void stl(EditText ek){
+        ek.setClickable(true);
+        ek.setCursorVisible(true);
+        ek.setFocusable(true);
+        ek.setFocusableInTouchMode(true);
     }
 
 }
